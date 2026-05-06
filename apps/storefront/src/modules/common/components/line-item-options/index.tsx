74e88355684a55be28bbe7979209ddbc372098c8
kunalrisaanva <kunalrisaanva12@gmail.com>
2026-05-06 16:54:17 +0530

    feat: exclude additional session and patient metadata keys from line item display

--- apps/storefront/src/modules/common/components/line-item-options/index.tsx
+++ apps/storefront/src/modules/common/components/line-item-options/index.tsx
@@ -10,9 +10,16 @@ type LineItemOptionsProps = {
 
 const HIDDEN_METADATA_KEYS = new Set([
   "is_booking",
+  "is_session",
   "booking_date",
   "booking_time",
   "booking_slot_iso",
+  "slot_iso_start",
+  "patient_first_name",
+  "patient_last_name",
+  "patient_email",
+  "patient_phone",
+  "event_slug",
   "is_auto_gift",
 ])
 
